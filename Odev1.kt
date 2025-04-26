72d97b669ee3cad0b8e344564a23a74f1435cdc2
Muhammed Kerim Çiçek <kerimcicek3455@gmail.com>
2025-04-27 02:02:02 +0300

    Create Odev1.kt

---
+++ Odev1.kt
@@ -0,0 +1,50 @@
+package com.example.androidstudiobootcamp02.odev1
+
+fun main() {
+    val sehir: String = "Izmır"
+    val ulke: String = "Turkiye"
+    val telefonNo = 5073822099L
+    val postaKodu = 35353
+    var email: String = "kerimcicek3455@gmail.com"
+    var meslek: String = "Ogrenci"
+    var stokMiktari = 1000
+    val musteriAdi = "Kerim"
+    var bakiye = 1000
+    val dogumGunu = "02.09.2004"
+    var medeniDurum = "Bekar"
+    var urunYorum = "Cok iyi"
+    val odemeTarihi = "11.04.2028"
+    val odeme = "1000TL"
+    var siparisAdeti = 1000
+    val aramaModeli = "Octovia"
+    val kitapAdi = "Kendime Dusunceler"
+    val yayimlanmaTarihi = "11.12.2009"
+    var indirimMiktari = 10
+    val odaSayisi = 4
+    var enlem = 3
+    var boylam = 5
+    var urunAdi = "Telefon"
+    var yemekFiyati = 270
+    val marka = "Nike"
+    val muzikAdi = "Karma"
+    val videoSuresi = 58
+    var urunPuani = 4
+    val resimAdi = "Mona Lisa"
+    var dosyaFormati = ".pip"
+    var renk = "Sari Lacivert"
+    val renkKodu = 3535
+    var telefonModeli = "Iphone 13"
+    var ekranBoyutu = 16
+    var agirlik = 78
+    val ulusalGun = "23 Nisan"
+    val tatilGunu = "Pazar"
+    var rezervasyonTarihi = "27.04.2025"
+    var sokakAdi = "Fevzi Çakmak"
+    var otobusHatti = 72
+    var kalanDakika = 12
+    val takipKodu = 345449494
+    var kuponSuresi = 24
+    val kuponKodu = 1242453
+    var faturaAdresi = "herbanhi bir ev"
+
+}
